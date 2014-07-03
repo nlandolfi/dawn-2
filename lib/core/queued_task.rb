5ab618f7d00f45e70ea0dbcb364bae69ab787b68
Nick Landolfi <nclandolfi@gmail.com>
2014-07-02 19:42:58 -0700

    add complete! to queued_task for explicitness

--- lib/core/queued_task.rb
+++ lib/core/queued_task.rb
@@ -5,11 +5,12 @@ module Dawn
     # --- Core {{{
 
     belongs_to :task
-    belongs_to :queue
 
     # --- }}}
 
     # --- Queue {{{
+
+    belongs_to :queue
 
     def current?; self.current; end
     alias_method :is_current?, :current?
@@ -20,6 +21,10 @@ module Dawn
     def incomplete; !complete?; end
     alias_method :incomplete?, :incomplete
     alias_method :is_incomplete?, :incomplete
+
+    def complete!
+      queue.mark_complete_task(self.task)
+    end
 
     # --- }}}
 
